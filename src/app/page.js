767b9714b388aa811e565bd5e5980d8f87123f9a
pushkar <pushkargupta606@gmail.com>
2025-09-10 22:49:10 +0530

    added the files

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+      Mindreader Enterprises is a biotech startup in its establishment phase, dedicated to strategic biotech market analysis and innovation. Our mission is to provide insights into emerging therapies, breakthrough research, and FDA clinical developments to guide investors and industry stakeholders in making informed decisions.
+      </main>
+      </div>
+
+  );
+}
